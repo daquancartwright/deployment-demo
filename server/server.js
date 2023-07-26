c8921ade2126ef106208e26c51572538a87c02c9
DaQuan Cartwright <daquancartwright@outlook.com>
2023-07-26 09:12:12 -0700

    hosting app

---
+++ server/server.js
@@ -0,0 +1,8 @@
+// npm init -y to get package .json
+
+const express = require('express')
+const app = express()
+
+app.use(express.static(`${__dirname}`))
+
+app.listen(4000, () => console.log(`Server running on 4000`))
